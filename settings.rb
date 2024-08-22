684357efadb11294f9c2ef3ef9a732dc73f75bdc
Wirt S <34081557+Wirt4@users.noreply.github.com>
2024-08-21 17:35:43 -0700

    removed some magic strings

--- settings.rb
+++ settings.rb
@@ -7,6 +7,7 @@ module Settings
   BACKGROUND = 'black'
   SQUARE_SIZE = 30
   TEXT_COLOR='white'
+  FOOD_COLOR=TEXT_COLOR
   WINNER_MSG_X = 70
   WINNER_MSG_Y = 350
   WINNER_Z_NDX = 5
@@ -18,4 +19,7 @@ module Settings
   PLAYER_TWO_COLORS = [Colors::FUCHSIA, Colors::BLUE, Colors::GREEN, ]
   START_Y_TAIL = 3
   START_LENGTH = 3
+  PRESS_SPACE ='(press space)'
+  TIE_MESSAGE='Tie'
+  WINNER_MESSAGE=' Wins'
 end
